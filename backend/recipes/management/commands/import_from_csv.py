21c6af9bc8581e450c7e76d99ddc9024bcc800ea
Андрей Долгодворов <andrey.dolgodvorov@megafon.ru>
2023-04-16 23:42:59 +1000

    1

--- backend/recipes/management/commands/import_from_csv.py
+++ backend/recipes/management/commands/import_from_csv.py
@@ -7,7 +7,7 @@ from ...models import (Ingredient)
 class Command(BaseCommand):
     def handle(self, **options):
         Ingredient.objects.all().delete()
-        with open(r'backend\data\ingredients.csv', encoding='utf-8') as file:
+        with open(r'data\ingredients.csv', encoding='utf-8') as file:
             reader = csv.reader(file)
             next(reader)
             for row in reader:
